29b3774573eb2eef39261d0d19a5713c32f63f5a
Pedro Seromenho <pedroseromenho@pedro.local>
2018-06-05 16:09:53 +0200

    var and functions

---
+++ ex_1/script.js
@@ -0,0 +1,53 @@
+var age = parseInt(prompt('Quel est ton age?'));
+
+if (18 <= age){
+    alert('Vous êtes majeur')
+} else {
+    alert('vous êtes mineur')
+};
+
+/*
+//Ex. 7
+var pointure = parseInt(prompt('Pointure?'));
+var year = parseInt(prompt('Année de naissaince?'));
+var resultat = alert(((((pointure * 2) + 5)) * 50 - year) + 1766);
+*/
+
+/*
+//Ex. 6
+var number1 = parseInt(prompt('entrer nombre virugle'));
+var number2 = parseInt(prompt('entrer encore nombre virugle'));
+var resultat = alert(number1 % number2);
+*/
+
+/*
+//Ex. 5
+var number1 = parseInt(prompt('entrer nombre virugle'));
+var number2 = parseInt(prompt('entrer encore nombre virugle'));
+var resultat = alert(number1 * number2);
+*/
+
+/*
+Ex. 4
+var nom = prompt('enter your surname');
+var prenom = prompt('enter your name');
+var ville = prompt('enter your city');
+
+alert("nom: " + nom + "\nprenom: " + prenom + "\nville: " + ville)
+*/
+
+/* 
+// Ex. 3
+var prenom = prompt('enter your name mother fucker');
+alert('Hey ' + prenom);
+*/
+
+/* 
+// Ex. 2
+
+var nom = 'Seromenho';
+var prenom = 'Pedro';
+var ville = 'Bruxelles';
+
+alert("nom: " + nom + "\nprenom: " + prenom + "\nville: " + ville)
+*/
---
+++ ex_2/ex01/ex01.js
@@ -0,0 +1,18 @@
+function surfaceRectangle() {
+   
+    var longueur = prompt('longueur svp');
+    var largeur = prompt('largeur svp');
+    var resultat = longueur * largeur;
+
+    alert(resultat);
+}
+
+function periRectangle() {
+  
+    var longueur = prompt('longueur svp');
+    var largeur = prompt('largeur svp');
+    var resultat = 2*(longueur * largeur);    
+
+    alert(resultat);
+}
+
